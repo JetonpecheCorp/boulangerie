8751143e2ce6b5f4086374741ed81afeff8e54bc
jetonpeche <nicolas.np63@gmail.com>
2025-08-11 23:10:24 +0200

    api test => groupe

---
+++ Api/tests/Api.test/GroupeTest.cs
@@ -0,0 +1,85 @@
+﻿namespace Api.test;
+
+public class GroupeTest
+{
+    string baseUrl = "api/groupe";
+
+    [Fact]
+    public async Task Lister_groupe()
+    {
+        await Connexion.AdministrateurAsync();
+
+        var reponse = await Connexion.HttpClient.GetAsync($"{baseUrl}/lister");
+
+        Assert.True(reponse.IsSuccessStatusCode);
+    }
+
+    [Theory]
+    [InlineData("grp 1", "adresse 1 ", HttpStatusCode.Created)]
+    [InlineData(null, null, HttpStatusCode.BadRequest)]
+    [InlineData("", "aaa", HttpStatusCode.BadRequest)]
+    [InlineData("grp 2", "", HttpStatusCode.BadRequest)]
+    public async Task Ajouter_groupe(string _nom, string _adresse, HttpStatusCode _codeRetour)
+    {
+        await Connexion.AdministrateurAsync();
+
+        var reponse = await Connexion.HttpClient.PostAsJsonAsync(
+            $"{baseUrl}/ajouter", new GroupeRequete(
+                _nom, 
+                _adresse
+            )
+        );
+
+        Assert.Equal(reponse.StatusCode, _codeRetour);
+    }
+
+    [Fact]
+    public async Task Bloquer_debloquer_groupe()
+    {
+        await Connexion.AdministrateurAsync();
+
+        var reponse = await Connexion.HttpClient.PostAsJsonAsync(
+            $"{baseUrl}/ajouter", new GroupeRequete(
+                "groupe 1",
+                "Adresse 1"
+            )
+        );
+
+        int idGroupe = (await reponse.Content.ReadFromJsonAsync<int>())!;
+
+        reponse = await Connexion.HttpClient.PutAsync(
+            $"{baseUrl}/bloquer-debloquer/{idGroupe}", null
+        );
+
+        Assert.True(reponse.IsSuccessStatusCode);
+
+        reponse = await Connexion.HttpClient.PutAsync(
+            $"{baseUrl}/bloquer-debloquer/{idGroupe}", null
+        );
+
+        Assert.True(reponse.IsSuccessStatusCode);
+    }
+
+    [Fact]
+    public async Task Modifier_groupe()
+    {
+        await Connexion.AdministrateurAsync();
+
+        var reponse = await Connexion.HttpClient.PostAsJsonAsync(
+            $"{baseUrl}/ajouter", new GroupeRequete(
+                "groupe 1",
+                "Adresse 1"
+            )
+        );
+
+        int idGroupe = (await reponse.Content.ReadFromJsonAsync<int>())!;
+
+        reponse = await Connexion.HttpClient.PutAsJsonAsync(
+            $"{baseUrl}/modifier/{idGroupe}", new GroupeRequete("grp modif", "adresse modif")
+        );
+
+        Assert.True(reponse.IsSuccessStatusCode);
+    }
+}
+
+public record GroupeRequete(string? Nom, string? Adresse);
