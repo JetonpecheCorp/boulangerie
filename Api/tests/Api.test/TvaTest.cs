4aed36c3a68678b9c67b64d68542f0c9470b437f
jetonpeche <nicolas.np63@gmail.com>
2025-08-11 21:34:42 +0200

    api test => tva

---
+++ Api/tests/Api.test/TvaTest.cs
@@ -0,0 +1,15 @@
+﻿namespace Api.test;
+public class TvaTest
+{
+    string baseUrl = "api/tva";
+
+    [Fact]
+    public async Task Lister_TVA()
+    {
+        await Connexion.AdministrateurAsync();
+
+        var reponse = await Connexion.HttpClient.GetAsync($"{baseUrl}/lister");
+
+        Assert.True(reponse.IsSuccessStatusCode);
+    }
+}
