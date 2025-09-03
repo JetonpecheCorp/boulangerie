e00029b4a4ebfd4e30e431ddbbfbb75beb501c37
jetonpeche <nicolas.np63@gmail.com>
2025-09-03 11:55:48 +0200

    api => methode extension enum
    personnalisé le nom

---
+++ Api/src/Api/Extensions/EnumExtension.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace Api.Extensions;
+
+public static class EnumExtension
+{
+    /// <summary>
+    /// Personnaliser le nom de l'enum
+    /// </summary>
+    /// <param name="_enum"></param>
+    /// <returns>Récupere la description dans <see cref="DescriptionAttribute"/> sinon le nom</returns>
+    public static string Description(this Enum _enum)
+    {
+        var champ = _enum.GetType().GetField(_enum.ToString());
+        var attribut = champ!.GetCustomAttribute<DescriptionAttribute>();
+
+        return attribut is null ? _enum.ToString() : attribut.Description;
+    }
+}
