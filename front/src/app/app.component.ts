39b0c3bd4c5727c68a9b48013124d1674204a7eb
jetonpeche <nicolas.np63@gmail.com>
2025-09-09 11:08:07 +0200

    front => app component btn deconnexion

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { BooleanInput } from '@angular/cdk/coercion';
 import { BreakpointObserver } from '@angular/cdk/layout';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatDrawerMode, MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { environment } from '../environments/environment';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalParametreComponent } from '@modal/modal-parametre/modal-parametre.component';
 
 @Component({
     selector: 'app-root',
@@ -17,6 +19,9 @@ import { environment } from '../environments/environment';
 })
 export class AppComponent 
 {
+  matDialog = inject(MatDialog);
+  router = inject(Router);
+
   mdcBackdrop: BooleanInput = false;
   drawerMode: MatDrawerMode = "push";
 
@@ -33,6 +38,17 @@ export class AppComponent
   protected EstConnecter(): boolean
   {
     return environment.utilisateur != undefined && environment.utilisateur != null;
+  }
+
+  protected Deconnexion(): void
+  {
+    sessionStorage.clear();
+    this.router.navigateByUrl("/");
+  }
+
+  protected OuvrirModalParametre(): void
+  {
+    this.matDialog.open(ModalParametreComponent);
   }
 
   private BreakpointChanges(): void 
