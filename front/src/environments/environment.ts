78585106211b3496016bc9662ee6bbe3000a4a97
jetonpeche <nicolas.np63@gmail.com>
2025-09-15 22:34:07 +0200

    Update environment.ts

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export var environment = {
-    urlApi: "http://localhost:5256/api",
+    urlApi: "http://localhost:5000/api",
     utilisateur: null as any,
     urlFront: ["localhost:4200", "localhost:9000"] as string[]
 };
